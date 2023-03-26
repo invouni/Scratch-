8643a08540128302ceaa3bfb6733becb4cf32df8
invouni <128811301+invouni@users.noreply.github.com>
2023-03-26 10:57:27 +0530

    Update LinkedList.java
    Signed-off-by: invouni <128811301+invouni@users.noreply.github.com>

--- LinkedList.java
+++ LinkedList.java
@@ -1,1 +1,20 @@
+public class Main {
 
+
+	private Node head;
+
+	static class Node {
+		int data;
+		Node next;
+		Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+
+	public static void main(String [] args) {
+		System.out.println("sab sahi hai");
+		Node root = new Node(89);
+	}
+}
