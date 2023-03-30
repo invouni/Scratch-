1dd9cf7ca167dbd1e52c2a5ba7971af256d13650
Vaibhav Gupta <128811301+invouni@users.noreply.github.com>
2023-03-30 20:53:10 +0530

    Update graph.java
    Signed-off-by: Vaibhav Gupta <128811301+invouni@users.noreply.github.com>

--- graph.java
+++ graph.java
@@ -1,13 +1,14 @@
 import java.util.*;
 
 public class Graph {
-//class edge
 	static class Edge {
 		int src;
 		int dest;
-		Edge(int src, int dest) {
+		int wt;
+		Edge(int src, int dest,int wt) {
 			this.src = src;
 			this.dest = dest;
+			this.wt = wt;
 		}
 	}
 
@@ -15,43 +16,42 @@ public class Graph {
 	static void creat(List<Edge> graph[]) {
 		for (int i = 0; i < graph.length; i ++)/*Initializing*/graph[i] = new ArrayList<Edge>();
 		//adding elems in graph
-		graph[0].add(new Edge(0, 1));
+		graph[0].add(new Edge(0, 1, 8));
 
-		graph[0].add(new Edge(0, 2));
+		graph[0].add(new Edge(0, 2, -1));
 
-		graph[1].add(new Edge(1, 0));
+		graph[1].add(new Edge(1, 0, 3));
 
-		graph[1].add(new Edge(1, 3));
+		graph[1].add(new Edge(1, 3, 7));
 
-		graph[2].add(new Edge(2, 0));
-		graph[2].add(new Edge(2, 4));
+		graph[2].add(new Edge(2, 0, 7));
+		graph[2].add(new Edge(2, 4, 9));
 
-		graph[3].add(new Edge(3, 1));
+		graph[3].add(new Edge(3, 1, 3));
 
-		graph[3].add(new Edge(3, 4));
+		graph[3].add(new Edge(3, 4, 6));
 
-		graph[3].add(new Edge(3, 5));
+		graph[3].add(new Edge(3, 5, 100));
 
-		graph[4].add(new Edge(4, 2));
+		graph[4].add(new Edge(4, 2, 0));
 
-		graph[4].add(new Edge(4, 3));
+		graph[4].add(new Edge(4, 3, 8));
 
-		graph[4].add(new Edge(4, 5));
+		graph[4].add(new Edge(4, 5, 7));
 
-		graph[5].add(new Edge(5, 3));
+		graph[5].add(new Edge(5, 3, 3));
 
-		graph[5].add(new Edge(5, 4));
+		graph[5].add(new Edge(5, 4, 6));
 
-		graph[5].add(new Edge(5, 6));
+		graph[5].add(new Edge(5, 6, 5));
 
-		graph[5].add(new Edge(6, 5));
+		graph[5].add(new Edge(6, 5, 3));
 
 	}
-        //printing graph
 	static void print(List<Edge> graph[]) {
 		for (int i = 0; i < graph.length; i ++) {
 			for (int j = 0; j < graph[i].size(); j++) {
-				System.out.print("[src is ‐> " + graph[i].get(j).src + " dest is -> " + graph[i].get(j).dest + "]");
+				System.out.print("[src is ‐> " + graph[i].get(j).src + " dest is -> " + graph[i].get(j).dest + " wt is -> " + graph[i].get(j).wt +" ]");
 			}
 			System.out.println();
 		}
